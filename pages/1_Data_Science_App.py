0a46f764d44357bc4a3029b3b05a38ba44c1a694
Shobhit-Singhh <117563572+Shobhit-Singhh@users.noreply.github.com>
2024-05-05 23:50:09 +0530

    Update 1_Data_Science_App.py

--- pages/1_Data_Science_App.py
+++ pages/1_Data_Science_App.py
@@ -95,10 +95,14 @@ def show_feature_weights_table(model, X_train, y_train):
     from scipy.stats import norm
     p_values = 2 * norm.cdf(-np.abs(z_values))
     
-    # Print coefficients, standard errors, z-values, and p-values
-    st.write("Coefficients, Standard Errors, Z-values, P-values:")
+    # Create a table to display the results
+    table_data = [["Feature", "Coefficient", "Standard Error", "Z-value", "P-value"]]
     for feature, coef, se, z, p in zip(["const"] + list(X_train.columns), model.coef_[0], std_err, z_values, p_values):
-        st.write(f"{feature}: {coef}, {se}, {z}, {p}")
+        table_data.append([feature, f"{coef:.2e}", f"{se:.2e}", f"{z:.2e}", f"{p:.2e}"])
+    
+    # Print the table
+    st.table(table_data)
+
 
 def compare_distribution(df, dummy, col=None):
     if col is None:
